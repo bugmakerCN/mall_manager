572878ce6b5501ad04dc313da79fcb8754ba20df
bugmakerCN <cssdivnbsp@163.com>
2018-11-05 20:59:57 +0800

    用户列表

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,101 +1,90 @@
 <template>
 <el-card class="box-card">
-
     <!-- 面包屑 -->
-    <el-breadcrumb separator="/">
-        <el-breadcrumb-item>首页</el-breadcrumb-item>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
         <el-breadcrumb-item>用户管理</el-breadcrumb-item>
         <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 搜索框 -->
     <el-row class="searchArea">
         <el-col :span="24">
-            <el-input placeholder="请输入内容" class="searchInput">
+            <el-input placeholder="请输入内容" v-model="input5" class="searchInput">
                 <el-button slot="append" icon="el-icon-search"></el-button>
             </el-input>
             <el-button type="success">添加用户</el-button>
         </el-col>
     </el-row>
     <!-- 表格 -->
-    <el-table
-     style="width: 100%">
-        <el-table-column
+    <el-table :data="tableData" stripe style="width: 100%">
+        <el-table-column 
         :type="index"
-        prop="date"
-        label="日期"
+        prop="date" 
+        label="日期" 
         width="100">
         </el-table-column>
 
-          <el-table-column
-        prop="date"
-        label="姓名"
+        <el-table-column 
+        prop="date" 
+        label="姓名" 
         width="100">
         </el-table-column>
-
-  <el-table-column
-        prop="date"
-        label="邮箱"
+        <el-table-column 
+        prop="date" 
+        label="邮箱" 
         width="100">
         </el-table-column>
-
-  <el-table-column
-        :type="index"
-        prop="date"
-        label="电话"
+        <el-table-column 
+        prop="date" 
+        label="电话" 
         width="100">
         </el-table-column>
-
-  <el-table-column
-        prop="date"
-        label="创建日期"
+        <el-table-column 
+        prop="date" 
+        label="创建日期" 
         width="80">
         </el-table-column>
-
-  <el-table-column
-        prop="date"
-        label="用户状态"
-        width="120">
+        <el-table-column 
+        prop="date" 
+        label="用户状态" 
+        width="60">
         </el-table-column>
-
-          <el-table-column
-        prop="date"
-        label="操作"
-        width="140">
+        <el-table-column 
+        prop="date" 
+        label="操作" 
+        width="90">
         </el-table-column>
-            </el-table>
-
+    </el-table>
     <!-- 分页 -->
-
 </el-card>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            tableData: [{
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-            }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-            }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄'
-            }, {
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
-            }]
-        }
+    export default {
+        data() {
+            return {
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+        },
+    created() {
+        this.loadTableData()
     },
-    created(){
-      this.loadTableData()
-    }
-    ,
     methods:{
       async loadTableData(){
         // 除了登录功能, 其他功能的接口都需要加入token 才能请求
@@ -109,8 +98,7 @@ export default {
     }
 
 
-
-}
+};
 </script>
 
 <style>
